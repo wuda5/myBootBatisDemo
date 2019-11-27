4b6ed609def419e8ae783cf1ada2bc15afba59f4
wuda5 <2252316187@qq.com>
2019-11-27 12:34:21 +0800

    修改---

---
+++ src/main/java/com/itheima/SpringbootMybatisApplication.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisApplication.class, args);
+		System.out.println(99);
+
+	}
+
+}
