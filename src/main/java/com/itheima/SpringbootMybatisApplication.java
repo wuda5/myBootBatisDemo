23f45b08d4390a67158b05f77f467cf760ca7f24
wuda5 <2252316187@qq.com>
2019-11-27 12:35:16 +0800

    修改---

--- src/main/java/com/itheima/SpringbootMybatisApplication.java
+++ src/main/java/com/itheima/SpringbootMybatisApplication.java
@@ -8,7 +8,7 @@ public class SpringbootMybatisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMybatisApplication.class, args);
-		System.out.println(99);
+		System.out.println(9999);
 
 	}
 
